2c627d34c01c491b7a9394de60c264e59af9aee9
francesco <fmarchio@redhat.com>
2017-04-24 15:17:25 +0200

    jsp changee

--- eap-hotrod/src/main/java/infinispan/TestServlet.java
+++ eap-hotrod/src/main/java/infinispan/TestServlet.java
@@ -2,16 +2,23 @@ package infinispan;
 
  
 
-import java.io.Console;
+ 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.UUID;
 
- 
 import javax.servlet.AsyncContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 
  
 
@@ -45,24 +52,56 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(value = "/test" )
 public class TestServlet extends HttpServlet {
 
- 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse response) {
-    // Here the request is put in asynchronous mode
-	  response.setContentType("text/html");
+  ArrayList<Player> list = new ArrayList<Player>();
 
+  private RemoteCacheManager cacheManager;
+  private RemoteCache<String, Object> cache;
+  
+  @Override
+  public void init() {
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      builder.addServer()
+            .host(System.getenv("HOTROD_SERVICE"))
+            .port(Integer.parseInt(System.getenv("HOTROD_SERVICE_PORT")));
+      cacheManager = new RemoteCacheManager(builder.build());
+      cache = cacheManager.getCache("default");
+      
+      System.out.println("Loaded Cache "+cache.getName());
+  }
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+	  doPost(req,res);
+  }
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse res) {
+    // Here the request is put in asynchronous mode
+	//  response.setContentType("text/html");
+
+	  String name = req.getParameter("name");
+	  String surname = req.getParameter("surname");
+	  String teamName = req.getParameter("teamname");
+	  
       // Actual logic goes here.
       PrintWriter out=null;
 	try {
-		out = response.getWriter();
+		//out = res.getWriter();
 		
-		  
-	        FootballManager manager = new FootballManager();
-	 
-            manager.printTeams(out);
-	     
-	             
- 
+		   Player player = new Player();
+		   player.setName(name);
+		   player.setSurname(surname);
+		   player.setTeamName(teamName);
+		   String randomId = UUID.randomUUID().toString();
+	       cache.put(randomId, player);
+	       
+          //set list in request scope and forward request to JSP
+            req.setAttribute("playerList",list);         
+            String nextJSP = "/index.jsp";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+            try {
+				dispatcher.forward(req,res);
+			} catch (ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	        
 	} catch (IOException e) {
 		// TODO Auto-generated catch block
--- eap-hotrod/src/main/webapp/index.jsp
+++ eap-hotrod/src/main/webapp/index.jsp
@@ -1,1 +1,20 @@
-Main Page
+ <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+ <h2>Welcome to EAP-JDG Demo</h2>
+ <h3>List of Objects in the cache:</h3>
+<c:forEach var="p" items="playerList">
+   <c:out value="${p.name}" />
+   <c:out value="${p.surname}" />
+   <c:out value="${p.teamName}" />
+ 
+</c:forEach>
+
+<br/>
+Add Data
+<br />
+<form action="test" >
+
+Name <input type="text" name="name" > <br/>
+Surname <input type="text" name="surname" > <br/>
+Team <input type="text" name="teamname" > <br/>
+<input type="submit">
+</form>
