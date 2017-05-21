788e39a02d74348093fc9a2ebaf22a6aa39ae114
francesco <fmarchio@redhat.com>
2017-05-21 19:01:25 +0200

    second change

--- eap-cluster/src/main/java/com/redhat/eapcluster/servlet/TestServlet.java
+++ eap-cluster/src/main/java/com/redhat/eapcluster/servlet/TestServlet.java
@@ -9,8 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.redhat.eapcluster.ejb.StatefulDemoEJB;
-import com.redhat.ejb.Service;
+import com.redhat.ejb.*;
 
 /**
  * Servlet implementation class TestServlet
